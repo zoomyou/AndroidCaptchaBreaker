4572c2a7209ade17d63e2d841c6658323b40f371
vagpoem <1084236503@qq.com>
2021-08-21 16:45:12 +0800

    feat: add button listen event

--- app/src/main/java/com/example/captchabreaker/MainActivity.java
+++ app/src/main/java/com/example/captchabreaker/MainActivity.java
@@ -6,7 +6,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
@@ -18,6 +20,10 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isOpen = false;
+    private boolean isWorking = false;
+    private String imageType = "0";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,44 +32,65 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageResource(R.drawable.test);
     }
 
-    public Bitmap getUrlBitmap(String url){
-        Bitmap bitmap = null;
+    public void openOrClose(View view){
+        // 开启 webSocket 开始接收任务
+        if (!isOpen()){
+            // TODO 开启任务接收
+        } else {
+            // TODO 关闭任务接收
+        }
+    }
 
-        try {
-            URL iconUrl = new URL(url);
-            URLConnection connection = iconUrl.openConnection();
-            HttpURLConnection URLConnection = null;
+    public void submit(View view){
 
-            if (url.charAt(4) == ':'){
-                URLConnection = (HttpURLConnection) connection;
-            }
-
-            if (url.charAt(4) == 's'){
-                URLConnection = (HttpsURLConnection) connection;
-            }
-
-            int length = URLConnection.getContentLength();
-
-            connection.connect();
-
-            InputStream inputStream = connection.getInputStream();
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, length);
-            bitmap = BitmapFactory.decodeStream(bufferedInputStream);
-            bufferedInputStream.close();
-            inputStream.close();
-        } catch (Exception e) {
-            System.out.println(e.getCause().getMessage());
+        if (!isOpen){
+            Toast.makeText(MainActivity.this, "未开始接收任务", Toast.LENGTH_LONG).show();
+            return;
         }
 
-        return bitmap;
+        if (!isWorking){
+            Toast.makeText(MainActivity.this, "当前无任务", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // TODO 提交任务结果
+        setWorking();
+
     }
 
-    public Bitmap getBase64Bitmap(String base64){
-        byte[] decodeString = Base64.decode(base64, Base64.DEFAULT);
+    public void abandon(View view){
 
-        Bitmap decodeByte = BitmapFactory.decodeByteArray(decodeString, 0, decodeString.length);
-
-        return decodeByte;
+        // TODO 放弃本次任务
+        Toast.makeText(MainActivity.this, "放弃", Toast.LENGTH_SHORT).show();
     }
 
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen() {
+        if (isOpen)
+            isOpen = false;
+        else
+            isOpen = true;
+    }
+
+    public boolean isWorking() {
+        return isWorking;
+    }
+
+    public void setWorking() {
+        if (isWorking)
+            isWorking = false;
+        else
+            isWorking = true;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
 }
